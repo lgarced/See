6c365ea0249f1103b4bc7aa6c497d938c8b91e56
Liberato Garced Torres <liberato.garced@gmail.com>
2023-02-22 08:21:11 -0500

    Fixed html next issue on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,24 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from '@/components/Header'
-// import Providers from "./Providers";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <html lang='en'>
-      <body>
-      <Head>
-        <title>See</title>
-        <meta name="description" content="See save your movies" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      </body>
-      </html>
-      );
+    <div>
+      <div>
+        <Providers>
+          <Head>
+            <title>See</title>
+            <meta name="description" content="See save your movies" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Header />
+        </Providers>
+      </div>
+    </div>
+  );
 }
