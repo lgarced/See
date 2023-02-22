f28a091a5bc527e0f29d80ffa12704492790d397
Liberato Garced Torres <liberato.garced@gmail.com>
2023-02-22 12:40:04 -0500

    Fixed error

--- src/pages/about/page.jsx
+++ src/pages/about/page.jsx
@@ -1,26 +1,12 @@
-Skip to content
-sahandghavidel
-/
-imdb
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-More
-imdb/src/app/about/page.jsx /
-@sahandghavidel
-sahandghavidel create the navbar component
- History
- 1 contributor
-36 lines (34 sloc)  1.71 KB
+
 export default function About() {
   return (
     <div className="max-w-6xl mx-auto space-y-4 p-4">
       <h1 className="text-2xl font-medium text-amber-600">About</h1>
       <p>
-        Welcome to our movie database website! 
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs.
       </p>
 
       <p>
