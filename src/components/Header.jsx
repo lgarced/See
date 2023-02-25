72e673c34370e4f81fbced099663c9de68458cc0
Liberato Garced Torres <liberato.garced@gmail.com>
2023-02-25 08:30:27 -0500

    No idea

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,11 @@ export default function Header() {
   return (
     <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
+      <DarkModeSwitch />
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
       <div className="flex items-center space-x-5">
-        <DarkModeSwitch />
         <Link href="/">
           <h2 className="text-2xl">
             <span className="font-bold bg-blue-500 py-1 px-2 rounded-lg mr-1">
